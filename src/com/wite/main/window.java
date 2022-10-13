867a57379b0333f65afc2259eac843f0881a5bc0
Nikolai McNeely <nikolai.mcneely15@gmail.com>
2022-10-13 00:30:59 -0700

    Implemented boilerplate window code

--- src/com/wite/main/window.java
+++ src/com/wite/main/window.java
@@ -1,5 +1,20 @@
 package com.wite.main;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 public class window {
 
+	JFrame _applicationWindow;
+	JTextArea _applicationTextField;
+	private String _applicationName = "WITE";
+	private float _applicationVersion = 0.1f;
+	
+	window()
+	{
+		// Create frame
+		_applicationWindow = new JFrame(_applicationName + " " + _applicationVersion);
+		// Text component
+		_applicationTextField = new JTextArea();
+	}
 }
