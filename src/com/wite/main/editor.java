ab0c2dee5a6f385847dbad55a8e231caf48ba7c0
Nikolai McNeely <nikolai.mcneely15@gmail.com>
2022-10-13 00:23:17 -0700

    Removed unused import

--- src/com/wite/main/editor.java
+++ src/com/wite/main/editor.java
@@ -2,7 +2,6 @@ package com.wite.main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.print.PrinterException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
